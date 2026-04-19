2f71375e4fbf5c1f39036f9b39b1a9ab3ed64c02
ubddprasanna <ubddprasannapro@gmail.com>
2026-04-19 19:14:32 +0530

    Implmented sub resource locater for sensors readings and state synch (part4)

--- src/main/java/com/smartcampus/model/SensorReading.java
+++ src/main/java/com/smartcampus/model/SensorReading.java
@@ -10,4 +10,35 @@ package com.smartcampus.model;
  */
 public class SensorReading {
 
+    private String id;        // UUID [cite: 94]
+    private long timestamp;   // Epoch time [cite: 95]
+    private double value;     // The actual reading [cite: 96]
+
+    public SensorReading() {
+        // Def constructor
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
 }
